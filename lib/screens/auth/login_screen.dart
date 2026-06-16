0c69d2e28fdd78f2fa96858444965136b669c69b
DivyPanchal11 <divy1200@gmail.com>
2026-06-16 23:34:30 +0530

    Updated login and registration UI

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:expanse_tracker_app/screens/auth/register_screen.dart';
 import '../../services/auth_service.dart';
+import '../home/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,82 +11,198 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool isLoading = false;
 
   final AuthService authService = AuthService();
 
-  final  TextEditingController emailController = TextEditingController();
-  final  TextEditingController passwordController = TextEditingController();
+  final TextEditingController emailController =
+  TextEditingController();
+
+  final TextEditingController passwordController =
+  TextEditingController();
 
   @override
-  void dispose(){
+  void dispose() {
     emailController.dispose();
     passwordController.dispose();
     super.dispose();
   }
 
+  Future<void> login() async {
+    if (emailController.text.trim().isEmpty ||
+        passwordController.text.trim().isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Please fill all fields"),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+
+    try {
+      setState(() {
+        isLoading = true;
+      });
+
+      final user = await authService.loginUser(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+
+      if (user != null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Login Successful"),
+            backgroundColor: Colors.green,
+          ),
+        );
+
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+        );
+      }
+    } catch (e) {
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            e.toString().replaceFirst('Exception: ', ''),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-      ),
+      backgroundColor: Color(0xFFF8FAFC),
+
       body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            Text("Smart Expence Tracker",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),),
+        padding: const EdgeInsets.all(20),
 
-            const SizedBox(height: 30,),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
 
-            TextField(
-              controller: emailController,
-              decoration: const InputDecoration(
-                  labelText: "Email",
-                  border: OutlineInputBorder()
-              ),
+
+                        CircleAvatar(radius: 40,backgroundColor: Color(0xFF0014A8),
+                          child: Icon(Icons.account_balance_wallet,
+                            size: 50,color: Color(0xFFF8FAFC)),
+                        ),
+                    SizedBox(height: 10,),
+                    const Text(
+                      "Expense Tracker",
+                      style: TextStyle(
+                        color: Color(0xFF0014A8),
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10,),
+                    const Text("Manage your expenses smartly",style: TextStyle(
+                      color: Colors.black
+                    ),),
+
+                    const SizedBox(height: 30),
+
+                    TextField(
+                      controller: emailController,
+                      decoration:  InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.35),
+                        prefixIcon: Icon(Icons.email,color: Color(0xFF0014A8),),
+                        hintText: "Email",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+
+                        ),
+                      ),
+
+                    ),
+
+                    const SizedBox(height: 15),
+
+                    TextField(
+                      controller: passwordController,
+                      obscureText: true,
+                      decoration:  InputDecoration(
+                        prefixIcon: Icon(Icons.lock,color: Color(0xFF0014A8),),
+                        hintText: "Password",
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.35),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(height: 25),
+
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        backgroundColor: Color(0xFF0014A8)
+                      ),
+                        onPressed: isLoading ? null : login,
+                        child: isLoading
+                            ? const SizedBox(
+                          height: 20,
+                          width: 20,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 2,
+                            color: Colors.white,
+                          ),
+                        )
+                            : const Text("Login",style: TextStyle(color: Color(0xFFF8FAFC),fontWeight: FontWeight.bold),),
+                      ),
+                    ),
+
+                    const SizedBox(height: 15),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text("Don't have an account?"),
+                        TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                const RegisterScreen(),
+                              ),
+                            );
+                          },
+                          child: const Text("Register",style: TextStyle(color: Color(0xFF0014A8),fontWeight: FontWeight.bold),),
+                        ),
+                      ],
+                    ),
+                  ],
             ),
-
-            const SizedBox(height: 15,),
-
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(
-                  labelText: "Password",
-                  border: OutlineInputBorder()
-              ),
-            ),
-
-            const SizedBox(height: 25),
-
-            SizedBox(
-                width: double.infinity,
-                child:ElevatedButton(onPressed: () async {
-
-                  final user  = await authService.loginUser(
-                      email: emailController.text.trim(),
-                      password: passwordController.text.trim()
-                  );
-
-                  if (user != null){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("Login Successful"))
-                    );
-                  }
-
-                }, child: const Text("Login"))),
-
-            const SizedBox(height: 15),
-
-            TextButton(onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>const RegisterScreen()));
-            }, child: const Text("Register"))
-
-          ],
+          ),
         ),
       ),
 
--- lib/screens/auth/register_screen.dart
+++ lib/screens/auth/register_screen.dart
@@ -5,81 +5,230 @@ class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
 
   @override
-  State<RegisterScreen> createState() => _RegisterScreenState();
+  State<RegisterScreen> createState() =>
+      _RegisterScreenState();
 }
 
-class _RegisterScreenState extends State<RegisterScreen> {
+class _RegisterScreenState
+    extends State<RegisterScreen> {
+  bool isLoading = false;
 
   final AuthService authService = AuthService();
 
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController nameController =
+  TextEditingController();
+
+  final TextEditingController emailController =
+  TextEditingController();
+
+  final TextEditingController passwordController =
+  TextEditingController();
 
   @override
-  void dispose(){
+  void dispose() {
     nameController.dispose();
     emailController.dispose();
     passwordController.dispose();
     super.dispose();
   }
 
+  Future<void> register() async {
+    if (nameController.text.trim().isEmpty ||
+        emailController.text.trim().isEmpty ||
+        passwordController.text.trim().isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Please fill all fields"),
+          backgroundColor: Colors.red,
+        ),
+      );
+      return;
+    }
+
+    try {
+      setState(() {
+        isLoading = true;
+      });
+
+      final user = await authService.registerUser(
+        email: emailController.text.trim(),
+        password: passwordController.text.trim(),
+      );
+
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+
+      if (user != null) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text("Registration Successful"),
+            backgroundColor: Colors.green,
+          ),
+        );
+
+        Navigator.pop(context);
+      }
+    } catch (e) {
+      if (mounted) {
+        setState(() {
+          isLoading = false;
+        });
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+            e.toString().replaceFirst(
+              'Exception: ',
+              '',
+            ),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Register",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
+      backgroundColor: const Color(0xFF0014A8), // Zaffre Blue
+
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Register",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold),),
-
-            const SizedBox(height: 25,),
-
-            TextField(
-              controller: nameController,
-              decoration: InputDecoration(labelText: "Name",border: OutlineInputBorder()),
+            const Padding(
+              padding: EdgeInsets.only(
+                left: 25,
+                top: 40,
+                right: 25,
+                bottom: 30,
+              ),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Create An\nAccount",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 34,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
             ),
 
-            const SizedBox(height: 15,),
+            Expanded(
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(25),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40),
+                    topRight: Radius.circular(40),
+                  ),
+                ),
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 35),
 
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(labelText: "Email",border: OutlineInputBorder()),
+                      TextField(
+                        controller: nameController,
+                        decoration:  InputDecoration(
+                            prefixIcon: Icon(Icons.person,color: Color(0xFF0014A8),),
+                            hintText: "Name",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15)
+                            )
+                        ),
+                      ),
+
+                      const SizedBox(height: 25),
+
+                      TextField(
+                        controller: emailController,
+                        decoration:  InputDecoration(
+                            prefixIcon: Icon(Icons.email,color: Color(0xFF0014A8),),
+                            hintText: "Email",
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15)
+                            )
+                        ),
+                      ),
+
+                      const SizedBox(height: 25),
+
+                      TextField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration:  InputDecoration(
+                          prefixIcon: Icon(Icons.lock,color: Color(0xFF0014A8),),
+                          hintText: "Password",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15)
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 50),
+
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFF0014A8),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15)
+                            ),
+                        ),
+                          onPressed:
+                          isLoading ? null : register,
+                          child: isLoading
+                              ? const SizedBox(
+                            height: 20,
+                            width: 20,
+                            child:
+                            CircularProgressIndicator(
+                              strokeWidth: 2,
+                              color: Colors.white,
+                            ),
+                          )
+                              : const Text(
+                            "Create Account",style: TextStyle(color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+
+                      const SizedBox(height: 25),
+
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+
+                          Text("Already a User?"),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text(
+                              "Login",
+                              style: TextStyle(
+                                color: Color(0xFF0014A8),
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-
-            const SizedBox(height: 15,),
-
-            TextField(
-              controller: passwordController,
-              decoration: InputDecoration(labelText: "Password",border: OutlineInputBorder()),
-            ),
-
-            const SizedBox(height: 25,),
-
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(onPressed: () async {
-
-                final user = await authService.registerUser(
-                    email: emailController.text.trim(),
-                    password: passwordController.text.trim()
-                );
-
-                if (user != null){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text("Registration Successful"))
-                  );
-                }
-
-              }, child: Text("Register")),
-            ),
-
-            const SizedBox(height: 15,),
-            TextButton(onPressed: (){Navigator.pop(context);}, child: Text("Alredy a User ? login"))
           ],
         ),
       ),
